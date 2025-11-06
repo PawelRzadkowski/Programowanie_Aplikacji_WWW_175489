ba6f8ff1a7495c2159a69d2e013fa6c811f3fe24
PawelRzadkowski <147385702+PawelRzadkowski@users.noreply.github.com>
2025-11-06 15:57:34 +0100

    add

---
+++ lab_5/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="">
+<head>
+<meta charset="UTF-8">
+<>
+</head>
+<body>
+<?
+  $nr_indeksu = ‘175489’;
+  $nrGrupy = ‘3’;
+  echo ‘Autor: Paweł Rzadkowski ‘.$nr_indeksu.’ grupa ‘.$nrGrupy.’ <br /><br />’;
+ 
+ error_reporting(E_ALL ^ E_NOTICE ^ E_WARNING);
+ /* po tym komentarzu będzie kod do dynamicznego ładowania stron */
+?>
+</body>
+</html>
